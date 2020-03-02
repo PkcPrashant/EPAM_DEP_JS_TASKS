99a87e49f1148aebc161b83a3bb596f44e819003
Prashant Choudhary <Prashant_Choudhary@epam.com>
2020-03-02 15:43:01 +0530

    renamed few variables

--- Task 2/Get The Middle Character.js
+++ Task 2/Get The Middle Character.js
@@ -2,9 +2,10 @@
 Question Link
 http://www.codewars.com/kata/get-the-middle-character
 */
-function getMiddle(myString) {
-  return myString.length % 2
-    ? myString.substring(myString.length / 2, myString.length / 2 + 1)
-    : myString.substring(myString.length / 2 - 1, myString.length / 2 + 1)
+function getMiddle(inputString) {
+  var strLengthByTwo = inputString.length / 2;
+  return inputString.length % 2
+    ? inputString.substring(strLengthByTwo, strLengthByTwo + 1)
+    : inputString.substring(strLengthByTwo - 1, strLengthByTwo + 1)
     ;
 }
--- Task 2/Word Count.js
+++ Task 2/Word Count.js
@@ -3,10 +3,10 @@ Question Link
 http://www.codewars.com/kata/word-count
 */
 
-function countWords(myString) {
-  myString = myString.trim();
-  return myString.length === 0
+function countWords(inputString) {
+  inputString = inputString.trim();
+  return inputString.length === 0
     ? 0
-    : myString.split(/\s+/g).length;
+    : inputString.split(/\s+/g).length;
 
 }
